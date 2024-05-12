4e3095a9bc1ba4513b11427a99f4fee84c6b778f
sarvagya-singh-pro <68740017+sarvagya230@users.noreply.github.com>
2024-05-12 14:48:01 +0530

    prayaas

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,72 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
-
+import { Josefin_Sans,Inter } from 'next/font/google'
+import { useRef } from "react";
+import useScrollSnap from "react-use-scroll-snap";
+import  Image  from "next/image";
+const josen=Josefin_Sans({ subsets: ['latin'] })
+const inter=Inter({ subsets: ['latin'] })
+import localFont from 'next/font/local'
+const logoFont = localFont({src:'/jsMath-cmr10.ttf'})
 export default function Home() {
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 50, delay: 20 });
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+<div ref={scrollRef}> 
+<nav className={styles.navbar}>
+  <div className={styles.logoDiv}>
+<Image src={'/logo.jpg'} width={50} height={50}/>
+<h1 className={`${styles.logo} ${josen.logo}`}>
+ 
+  
+  Prayas
+  </h1>
+  </div>
+
+  <ul>
+
+    <li>Home</li>
+    <li>Donate</li>
+    <li>Campaings</li>
+    <li>Team</li>
+  </ul>
+</nav>
+<div className={styles.landing}>
+  <h1 className={`${logoFont.className}`}>Prayaas</h1>
+ 
+  <h2  className={`${styles.intro} ${josen.className}`}>Create.<span>Help.</span><span>Explore</span></h2>
+  <h3>
+  Prayass Is a community of highly motivated students.we are large group students not as many as her exs but still large 
+  </h3>
+  <div className={styles.sideImage}>
+    <div className={styles.container}>
+      <div className={styles.mainRhombus}>
+        <div className={styles.logoImageContainer}>
+        <Image src={'/logo.svg'} width={400} height={400} />
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
+    <div className={styles.sideRhombus}>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    </div>
+    <div className={styles.sideRhombus2}>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    </div>
+  </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+</div>
+<div className={styles.visionPage}>
+<div className={styles.tint}>
+<h1 className={`${styles.vision} ${inter.vision}`}>Our Vision</h1>
+<h2 className={`${styles.visionText} ${inter.visionText}`}>
+  Our aim is do your mom lmao,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</h2>
+  </div>
+</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+</div>
+
   );
 }
