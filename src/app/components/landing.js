81894f80e481d0c767e600ab9e2360e9f63cf211
sarvagya-singh-pro <68740017+sarvagya230@users.noreply.github.com>
2024-05-15 15:06:03 +0530

    campaigns

--- src/app/components/landing.js
+++ src/app/components/landing.js
@@ -2,8 +2,13 @@
 import styles from "./page.module.css";
 import { Josefin_Sans,Inter } from 'next/font/google'
 import { useRef ,useState,useEffect} from "react";
+import { Carousel } from '@mantine/carousel';
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Drawer } from "@mantine/core";
 import useScrollSnap from "react-use-scroll-snap";
+import Wave from 'react-wavify'
+import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css'
 import  Image  from "next/image";
 const josen=Josefin_Sans({ subsets: ['latin'] })
 const inter=Inter({ subsets: ['latin'] })
@@ -20,7 +25,7 @@ function getWindowDimensions() {
 }
 export default function Home() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
+    const[drawer,Setdrawer]=useState(false)
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
@@ -30,18 +35,24 @@ export default function Home() {
     return () => window.removeEventListener('resize', handleResize);
     }
   }, []);
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 50, delay: 20 });
   return (
 
-<div ref={scrollRef}> 
+
+
+<div > 
+
+<Drawer  opened={drawer} size={"md"} position="right" onClose={()=>{Setdrawer(false)}}>
+    <h1>Nigga</h1>
+
+</Drawer>
 <nav className={styles.navbar}>
+
   <div className={styles.logoDiv}>
 <Image src={'/logo.jpg'} width={50} height={50}/>
 <h1 className={`${styles.logo} ${josen.logo}`}>
  
   
-  Prayas
+  Prayaas
   </h1>
   </div>
         {windowDimensions.width>1000?
@@ -51,13 +62,13 @@ export default function Home() {
     <li>Donate</li>
     <li>Campaings</li>
     <li>Team</li>
-  </ul>:<RxHamburgerMenu size={"2rem"} className={styles.hamburger} />
+  </ul>:<RxHamburgerMenu onClick={()=>{Setdrawer(!drawer)}} size={"2rem"} className={styles.hamburger} />
 }
 </nav>
 <div className={styles.landing}>
   <h1 className={`${logoFont.className}`}>Prayaas</h1>
  
-  <h2  className={`${styles.intro} ${josen.className}`}>Create &#9679;<span>Help  &#9679;</span><span>Explore</span></h2>
+  <h2  className={`${styles.intro} ${josen.className}`}>Create &#9679;<span>Help  &#9679;</span><span>Explore &#9679;</span></h2>
   <h3>
   Prayass Is a community of highly motivated students.we are large group students not as many as her exs but still large 
   </h3>
@@ -83,11 +94,44 @@ export default function Home() {
 <div className={styles.tint}>
 <h1 className={`${styles.vision} ${inter.vision}`}>Our Vision</h1>
 <h2 className={`${styles.visionText} ${inter.visionText}`}>
-  Our aim is do your mom lmao,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Our aim is do your mom lmao, and become a better human than her but that wont be hard Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi .
 </h2>
   </div>
 </div>
+<div className={styles.donate}>
+    <h1 className={`${styles.donateText} ${inter.vision}`}>Donate</h1>
+    <h2>Please donate we need money and unlike her we cant even strippe</h2>
+    <button className={styles.donateBtn}><span className={styles.donateBtnText}>Donate</span></button>
+    <Image src={'/donate.svg'} className={styles.donateImg} width={400} height={400}>
+    </Image>
+    <Wave fill='#DB0007'
+        paused={false}
+        className={styles.wave}
+        options={{
+          height: 5,
+          amplitude: 20,
+          speed: 0.15,
+          points: 3
+        }}
+  />
+</div>
+<div className={`${styles.campaings} ${josen.className}`}>
+    <h1>Campaigns</h1>
+    <div className={styles.campaignContent}>
+        <Carousel  slideSize={{ base: '100%', sm: '50%' }}
+      slideGap={{ base: 'xl', sm: 2 }} loop   className={styles.carousel} withIndicators w={350} height={300}>
+        <Carousel.Slide><Image width={450} height={300} src={"https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_640.png"}></Image></Carousel.Slide>
+        <Carousel.Slide><Image width={450} height={300} src={"https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_640.png"}></Image></Carousel.Slide>
+       
+         </Carousel>
+    <h3 className={inter.className}>
+        In our campaings we found that she will ride anything with a pulse.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan, lorem vitae aliquam facilisis, elit ante pulvinar lorem, eget ullamcorper nisi sapien vel lectus
+    </h3>
+    </div>
 
+
+</div>
 </div>
 
   );
