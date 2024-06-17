1487a2ad55e04d922783571cd9fbac06ce936020
sarvagya-singh-pro <68740017+sarvagya230@users.noreply.github.com>
2024-06-18 01:25:06 +0530

    open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,6 @@ import { createTheme, MantineProvider } from '@mantine/core';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Praayas",
-  description: "Praayas is a community of passionate students, attempting to make the world around them a better place.",
-};
 
 export default function RootLayout({ children }) {
   return (
@@ -18,7 +14,12 @@ export default function RootLayout({ children }) {
         <meta name = "keywords" content = "Non-profit, help, student-run, support,help"/>
         <meta property="og:title" content="Prayaas" />
         <meta property="og:description" content="Prayaas is a student run  community service non-profit based in bokaro join and help improve our city for better "/>
-        <meta property="og:image" content="https://i.ibb.co/kybh2h2/Whats-App-Image-2024-06-18-at-00-47-28-d4d7f497.jpg" />
+        <meta property="og:image" content="/og.jpg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Prayaas" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:video" content="/video.mp4" />
+        <meta property="og:url" content="https://www.prayaas.org.in/" />
       </head>
       <body className={inter.className}>
         <MantineProvider>{children}
